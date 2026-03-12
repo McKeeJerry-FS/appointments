27dfbfd14c7d89c8e32348429e92a8cd18eae980
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2026-03-11 22:17:43 -0400

    added blankCusyomer

--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -3,19 +3,23 @@ import { initializeReactContainer, render, element, form } from '../test/reactTe
 import { CustomerForm } from '../src/CustomerForm';
 
 describe('CustomerForm', () => {
+  const blankCustomer = {
+    firstName: '',
+  };
+  
   beforeEach(() => {
     initializeReactContainer();
   });
 
   // Test for a form rendering
   it('renders a form', () => {
-    render(<CustomerForm />);
+    render(<CustomerForm original={blankCustomer} />);
     expect(form()).not.toBeNull();
   });
 
   // Test for rendering the first name input field
   it('renders a first name input field', () => {
-    render(<CustomerForm />);
+    render(<CustomerForm original={blankCustomer} />);
     const field = form().elements.firstName;
     expect(field).not.toBeNull();
     expect(field.tagName).toEqual('INPUT');
