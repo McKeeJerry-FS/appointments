ba1925ec7e774326f1d489c588476dea7397efc9
Jerry McKee Jr <jerry.mckee.jr0882@gmail.com>
2026-03-12 13:26:50 -0400

    creating the tests for submitting a form

--- src/CustomerForm.js
+++ src/CustomerForm.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-export const CustomerForm = ({ original = {} }) => (
-  <form>
+export const CustomerForm = ({ original, onSubmit }) => (
+  <form onSubmit={() => onSubmit(original)}>
     <label htmlFor="firstName">First Name</label>
-    <input type="text" name="firstName" value={original.firstName} />
+    <input type="text" name="firstName" id="firstName" value={original.firstName} />
+    <button type="submit">Submit</button>
   </form>
 );
--- test/CustomerForm.test.js
+++ test/CustomerForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { initializeReactContainer, render, element, form, field } from '../test/reactTestExtensions';
+import { initializeReactContainer, render, element, form, field, click } from '../test/reactTestExtensions';
 import { CustomerForm } from '../src/CustomerForm';
 
 describe('CustomerForm', () => {
@@ -36,6 +36,40 @@ describe('CustomerForm', () => {
     render(<CustomerForm original={blankCustomer} />);
     const label = element('label[for="firstName"]');
     expect(label).not.toBeNull();
-    expect(label.textContent).toMatch(/first\s*name/i);
+    expect(label.textContent).toMatch("First Name");
   });
-})
+
+  it('renders First Name as the label for the field', () => {
+    render(<CustomerForm original={blankCustomer} />);
+    const label = element('label[for="firstName"]');
+    expect(label).not.toBeNull();
+    expect(label.textContent).toBe('First Name');
+  });
+
+  it('assigns an id that matches the label id to the first name field', () => {
+    render(<CustomerForm original={blankCustomer} />);
+    expect(field("firstName").id).toEqual("firstName");
+  });
+
+  // These tests will be for submitting the customer Form
+
+  // test for rendering a submit button
+  it('renders a submit button', () => {
+    render(<CustomerForm original={blankCustomer} />);
+    const submitButton = element('button[type="submit"]');
+    expect(submitButton).not.toBeNull();
+  });
+
+  // test for saving the existing first name when submitted
+  // introduces the concept of "assertions" and "expect.hasAssertions()"
+  it('saves existing first name when submitted', () => {
+    expect.hasAssertions();
+    const customer = { firstName: 'Ashley' };
+    render(<CustomerForm original={customer}
+                         onSubmit={({ firstName }) => {
+                           expect(firstName).toEqual('Ashley');
+                         }} />);
+    const submitButton = element('button[type="submit"]');
+    click(submitButton);
+  });
+});
